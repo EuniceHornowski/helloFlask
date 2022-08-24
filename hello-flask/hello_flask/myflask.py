993b0205a58981152889498ecc233a124ebf6fb6
Eunice <eunice.hornowski@le-campus-numerique.fr>
2022-08-24 17:00:47 +0200

    modifs2

--- hello-flask/hello_flask/myflask.py
+++ hello-flask/hello_flask/myflask.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route("/")
 def home():
+
 	return render_template('index.html')
 	
 @app.route('/about')
@@ -10,6 +11,20 @@ def about():
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
+	if request.method == 'GET':
+		return render_template('form.html')
+	elif request.method == 'POST':
+		form_data_f = request.form['fname']
+		form_data_l = request.form['lname']
+
+		with open("dataset.txt", "a") as f:
+			f.write(form_data_f)
+			f.write(';')
+			f.write(form_data_l)
+			f.write('\n')
+
+
+		return render_template('form.html', titre = 'abcd')
 	return render_template('form.html')
 
 @app.route('/gateaux')
